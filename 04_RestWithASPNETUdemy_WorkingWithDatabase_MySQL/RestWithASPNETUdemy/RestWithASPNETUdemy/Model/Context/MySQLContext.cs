b51d68ee3753ba0fc022618d81a0b62e539ab15f
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-04-01 10:16:21 -0300

    Working With Database MySQL

---
+++ 04_RestWithASPNETUdemy_WorkingWithDatabase_MySQL/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
