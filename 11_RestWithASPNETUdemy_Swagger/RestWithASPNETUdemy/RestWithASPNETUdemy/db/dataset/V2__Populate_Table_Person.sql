70fd078d9870541f60b885f5cb05497fb6b90cd3
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-04-03 08:53:18 -0300

    Swagger Implementation and Customization

---
+++ 11_RestWithASPNETUdemy_Swagger/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `last_name`, `gender`) VALUES
+	(1, 'Italia', 'Leonardo', 'da Vinci', 'Male'),
+	(2, 'Belo Horizonte', 'Alexandre', 'Paim', 'Male'),
+	(3, 'Belo Horizonte', 'Patricia', 'Galacho', 'Female'),
+	(4, 'BH', 'Berenice', 'Soares Safar Paim', 'female');
